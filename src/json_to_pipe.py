8054a4414b2d1e625f6e07ef5855559217f50730
al3kq <al435andrewlee@gmail.com>
2024-01-13 01:25:58 -0500

    json rewrite

--- src/json_to_pipe.py
+++ src/json_to_pipe.py
@@ -4,28 +4,17 @@ from editors.clipping.random_clip_editor import RandomClipEditor
 from editors.formatting.aspect_ratio_format_editor import AspectRatioFormatter
 from editors.captioning.caption_adder import CaptionAdder
 from pipeline.pipeline_execute import execute_pipeline_from_dir
-from utils.video_utils import match_and_combine_clips
-from io_files.video_writer import save_video_files
-import pydantic
-from pipeline.pipeline_execute import execute_pipeline_from_dir
 from io_files.video_writer import save_video_files
 
-top_bottom_clips = []
-
-# Path to your JSON file
-json_file_path = 'example_json/main2_json.json'
-
-all_data = None
-# Open the file and load JSON content
-with open(json_file_path, 'r') as file:
-    all_data = json.load(file)
 # Function to create an editor instance based on the type and parameters
-    
-print(all_data)
 def create_editor(editor_data):
     editor_type = editor_data['type']
     if editor_type == 'RandomClipEditor':
-        return RandomClipEditor(start_time=editor_data.get('start', 0), duration=editor_data.get('end', 30), silent=editor_data.get('silent', False))
+        return RandomClipEditor(
+            start_time=editor_data.get('start', 0),
+            duration=editor_data.get('end', 30),
+            silent=editor_data.get('silent', False)
+        )
     elif editor_type == 'AspectRatioFormatter':
         return AspectRatioFormatter(aspect_ratio=editor_data['aspect_ratio'])
     elif editor_type == 'CaptionAdder':
@@ -33,21 +22,18 @@ def create_editor(editor_data):
     else:
         raise ValueError(f"Unknown editor type: {editor_type}")
 
-# Process Pipeline
+# Load JSON data from file
+json_file_path = 'example_json/main2_json.json'
+with open(json_file_path, 'r') as file:
+    all_data = json.load(file)
 
+# Process and setup the pipeline
 pipeline_data = all_data["pipeline"]
-tasks = []
-name = ""
-pipeline = None
-out_dir = ""
-in_dir = pipeline_data["directory"]
+pipeline_tasks = [create_editor(task) for task in pipeline_data["tasks"]]
+pipeline = VideoEditingPipeline(name=pipeline_data["name"], tasks=pipeline_tasks)
 
-out_dir = all_data["output_directory"]
-name = pipeline_data["name"]
-for task in pipeline_data["tasks"]:
-    tasks.append(create_editor(task))
-
-pipeline = VideoEditingPipeline(name=name, tasks=tasks)
-
-clip = execute_pipeline_from_dir(in_dir, pipeline)
-save_video_files(clip, out_dir)
+# Execute the pipeline and save the output
+input_directory = pipeline_data["directory"]
+output_directory = all_data["output_directory"]
+clip = execute_pipeline_from_dir(input_directory, pipeline)
+save_video_files(clip, output_directory)
