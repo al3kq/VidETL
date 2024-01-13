725faafc605f6f9906b68c9c48664b3fa1fbf83f
al3kq <al435andrewlee@gmail.com>
2024-01-13 01:22:16 -0500

    dirty nonplural json_to_pipe example

---
+++ src/json_to_pipe.py
@@ -0,0 +1,53 @@
+import json
+from pipeline.video_pipeline import VideoEditingPipeline
+from editors.clipping.random_clip_editor import RandomClipEditor
+from editors.formatting.aspect_ratio_format_editor import AspectRatioFormatter
+from editors.captioning.caption_adder import CaptionAdder
+from pipeline.pipeline_execute import execute_pipeline_from_dir
+from utils.video_utils import match_and_combine_clips
+from io_files.video_writer import save_video_files
+import pydantic
+from pipeline.pipeline_execute import execute_pipeline_from_dir
+from io_files.video_writer import save_video_files
+
+top_bottom_clips = []
+
+# Path to your JSON file
+json_file_path = 'example_json/main2_json.json'
+
+all_data = None
+# Open the file and load JSON content
+with open(json_file_path, 'r') as file:
+    all_data = json.load(file)
+# Function to create an editor instance based on the type and parameters
+    
+print(all_data)
+def create_editor(editor_data):
+    editor_type = editor_data['type']
+    if editor_type == 'RandomClipEditor':
+        return RandomClipEditor(start_time=editor_data.get('start', 0), duration=editor_data.get('end', 30), silent=editor_data.get('silent', False))
+    elif editor_type == 'AspectRatioFormatter':
+        return AspectRatioFormatter(aspect_ratio=editor_data['aspect_ratio'])
+    elif editor_type == 'CaptionAdder':
+        return CaptionAdder()
+    else:
+        raise ValueError(f"Unknown editor type: {editor_type}")
+
+# Process Pipeline
+
+pipeline_data = all_data["pipeline"]
+tasks = []
+name = ""
+pipeline = None
+out_dir = ""
+in_dir = pipeline_data["directory"]
+
+out_dir = all_data["output_directory"]
+name = pipeline_data["name"]
+for task in pipeline_data["tasks"]:
+    tasks.append(create_editor(task))
+
+pipeline = VideoEditingPipeline(name=name, tasks=tasks)
+
+clip = execute_pipeline_from_dir(in_dir, pipeline)
+save_video_files(clip, out_dir)
